115881944d571550d06ff814d25fa6e8158582bc
2gaudy <agrant9946@gmail.com>
2022-10-10 23:01:10 -0400

    Added NoteInput and InputProvider

---
+++ src/components/NoteInput.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import NoteInput from '../providers/InputProvider';
+import NotesList from './providers/InputProvider';
+
+const NoteContext = React.createContext();
+
+const NotesInput = () => {
+
+
+    
+    const [note, setNote] = React.useState('Enter note here: ');
+
+    function AddNote(note){
+        setState(e.target.value)
+        
+    }
+    
+    function clickHandler(){
+        
+    }
+
+    return(
+        <div>
+            <input className="input" onChange={AddNote}/>
+            <p>Your Note: {note}</p>
+            <button onClick={clickHandler}>Save Note</button>
+        </div>
+    );
+}
+
+export const useNoteContext = () => React.useContext(NoteContext)
+export default NoteInput;
---
+++ src/providers/InputProvider.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const InputProvider = React.createContext();
+
+const NoteList = {
+    "list": ["Test"]
+}
+
+function InputProvider({children}) {
+    
+    const {NoteList, setNoteList} = useState(NoteList)
+
+    return(
+    <InputProvider.Provider value={NoteList["list"]}>
+        {children}
+    </InputProvider.Provider>
+    );
+}
+
+export const useInputContext = () => React.useContext();
+export default NoteInput;
